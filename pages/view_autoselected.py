fd97f1145b87d45a14b908c3a8877072b7262aa5
Vivien Nguyen <vivienn@princeton.edu>
2022-12-15 14:23:04 -0500

    Add some text to pages

--- pages/view_autoselected.py
+++ pages/view_autoselected.py
@@ -7,6 +7,13 @@ with open('models.txt') as f:
 
 exps = [5,35,65]
 
+
+st.text('Each row shows a different model and scale [5,35,65].')
+st.text('The first image is the original drawing, and the final drawing is the final image in the simplification sequence, i.e. if every possible simplification according to region constraints is made.')
+st.text('The middle image shows an "automatically selected" image based on the combined loss function (LPIPS+Density -- see loss curves in view_plots).')
+st.text('The index is computed as follows:')
+st.text('Suppose we have a sequence of loss values, L. Find the location where L(t+1) - L(t) > threshold')
+st.text('threshold is set to max(L)*.0005*scale')
 cols = st.columns(3)
 for m_name in models:
     for exp in exps:
---
+++ pages/view_plots.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(layout='wide')
+
+with open('models.txt') as f:
+    models = f.read().splitlines()
+
+st.text('Plots per model of the summed loss function (LPIPS+Density).')
+st.text('Note that there is some pre-processing of each line drawing before the algorithm is run. This pre-processing depends on the scale parameter, so index=0 of each curve does not correspond to the exact same drawing.')
+try:
+    st.image(f'plots/{m_name}.png')
