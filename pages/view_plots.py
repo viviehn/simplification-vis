a69df5e2e064cbafa8c17d495c0e4d2fbc97ff3e
Vivien Nguyen <vivienn@princeton.edu>
2022-12-15 14:55:07 -0500

    Fix plot page

--- pages/view_plots.py
+++ pages/view_plots.py
@@ -7,5 +7,8 @@ with open('models.txt') as f:
 
 st.text('Plots per model of the summed loss function (LPIPS+Density).')
 st.text('Note that there is some pre-processing of each line drawing before the algorithm is run. This pre-processing depends on the scale parameter, so index=0 of each curve does not correspond to the exact same drawing.')
-try:
-    st.image(f'plots/{m_name}.png')
+for m_name in models:
+    try:
+        st.image(f'plots/{m_name}.png')
+    except:
+        continue
